c5a658014809369cea61d3f885cdb644712857ac
Hyeeun Mok <hyeeuni.mok@gmail.com>
2020-02-08 23:20:06 -0500

    useState Hook

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid from 'uuid/v1';
 
 const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: 'Last of the American Girls', id: 1 },
+    { title: 'American Idiot', id: 2 },
+    { title: 'Basket Case', id: 3 }
+  ]);
+  const addSong = () => {
+    setSongs([
+      ...songs, { title: 'new song', id: uuid()}
+    ]);
+  }
   return (
     <div className="song-list">
       <ul>
-        <li>Last of the American Girls</li>
-        <li>American Idiot</li>
-        <li>Basket Case</li>
+        {songs.map(song => {
+          return ( <li key={song.id}>{song.title}</li>);
+        })}
       </ul>
+      <button onClick={addSong}>Add a song</button>
     </div>
   );
 }
