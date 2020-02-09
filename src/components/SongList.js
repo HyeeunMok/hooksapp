c3515cfb373f173e1548d56147b66ffd388817bf
Hyeeun Mok <hyeeuni.mok@gmail.com>
2020-02-08 23:00:08 -0500

    initial commit

---
+++ src/components/SongList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SongList = () => {
+  return (
+    <div className="song-list">
+      <ul>
+        <li>Last of the American Girls</li>
+        <li>American Idiot</li>
+        <li>Basket Case</li>
+      </ul>
+    </div>
+  );
+}
+
+export default SongList;
